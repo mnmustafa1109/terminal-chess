c05badd2cb6cc5190b2056519d80d09bfbcd08e8
mnmustafa1109 <mnmustafa1109@gmail.com>
2021-12-27 13:39:25 +0500

    run.sh upgraded

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 #    Section : SE-Q 2021
 #    ROll No : 21I -1235 
 #    PF   PROJECT   2021
-g++ -g main.cpp init_win.cpp init_colours.cpp calc.cpp piece.cpp piece_man.cpp write.cpp move_rule.cpp move_piece.cpp movement.cpp obs_rec.cpp score.cpp -o terminal_chess -lncurses
+g++ -g main.cpp init_win.cpp init_colours.cpp calc.cpp piece.cpp piece_man.cpp write.cpp move_rule.cpp move_piece.cpp movement.cpp obs_rec.cpp score.cpp history.cpp -o terminal_chess -lncurses
 ./terminal_chess
