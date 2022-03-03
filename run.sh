6ae99bab9b8bbe3a43c87f66d898fdace57c27cd
mnmustafa1109 <47169052+mnmustafa1109@users.noreply.github.com>
2022-03-03 22:40:04 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-#    Noman Mustafa Mehar
-#    Section : SE-Q 2021
-#    ROll No : 21I -1235 
-#    PF   PROJECT   2021
-g++ -g source/main.cpp source/init_win.cpp source/init_colours.cpp source/calc.cpp source/piece.cpp source/piece_man.cpp source/write.cpp source/move_rule.cpp source/move_piece.cpp source/movement.cpp source/obs_rec.cpp source/score.cpp source/history.cpp -o terminal_chess -lncurses
-./terminal_chess
+cd source
+g++ -c *.cpp  -lncurses
+g++ *.o -o terminal-chess -lncurses
+rm -r *.o
+./terminal-chess
+cd ..
+
